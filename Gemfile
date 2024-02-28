3c6b83cdfafbf70c62e8b0e4621cd96395a9e6e8
Kevin <kevin.calva@us.isobar.com>
2024-02-27 20:19:05 -0600

    adding new libraries to gemfile, creating a ruby app that displays api json response of random recipes, and added a erb file for the html.

--- Gemfile
+++ Gemfile
@@ -1,10 +1,14 @@
 source "https://rubygems.org"
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
-ruby "3.2.1"
+ruby "2.6.10"
 
 gem "sinatra"
 gem "sinatra-contrib"
+
+gem 'http'
+gem 'json'
+gem 'dotenv'
 
 # Use Puma as the app server
 gem "puma", "~> 5.0"
--- app.rb
+++ app.rb
@@ -1,9 +1,40 @@
 require "sinatra"
 require "sinatra/reloader"
+require 'json'
+require 'http'
+require 'dotenv/load'
 
-get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+class RandomRecipeGenerator
+  API_BASE_URL = 'https://api.spoonacular.com/recipes/random'
+
+  def initialize(api_key)
+    @api_key = api_key
+  end
+
+  def get_random_recipe
+    uri = URI("#{API_BASE_URL}?apiKey=#{@api_key}")
+    response = HTTP.get(uri)
+    parse_recipe(JSON.parse(response))
+  end
+
+  private
+
+  def parse_recipe(response)
+    recipe = response['recipes'].first
+    ingredients = recipe['extendedIngredients'].map { |ingredient| ingredient['original'] }
+    {
+      title: recipe['title'],
+      ingredients: ingredients,
+      instructions: recipe['instructions'],
+      image: recipe['image']
+    }
+  end
 end
+
+API_KEY = ENV['API_KEY']
+generator = RandomRecipeGenerator.new(API_KEY)
+
+get('/'){
+  @recipe = generator.get_random_recipe
+  erb(:index)
+}
